fc8a8d34307e1b336f82dd016ce917417fb5e948
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-04-15 18:38:29 -0400

    add new/0, new/1 to Chat module

--- lib/sketchy_chat/chat.ex
+++ lib/sketchy_chat/chat.ex
@@ -4,6 +4,29 @@ defmodule SketchyChat.Chat do
   """
 
   @doc """
+  Creates a new chat with a randomly generated name.
+  Returns {:ok, server_name} on success or {:error, reason} on failure.
+  """
+  @spec new() :: {:ok, String.t()} | {:error, term()}
+  def new() do
+    name = SketchyChat.Utils.ShortCode.generate()
+    new(name)
+  end
+
+  @doc """
+  Creates a new chat with the specified name.
+  Returns {:ok, server_name} on success or {:error, reason} on failure.
+  """
+  @spec new(String.t()) :: {:ok, String.t()} | {:error, term()}
+  def new(name) when is_binary(name) do
+    case SketchyChat.Chat.Server.start_link(name) do
+      {:ok, _pid} -> {:ok, name}
+      {:error, {:already_started, _pid}} -> {:error, :already_exists}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @doc """
   Appends a new message to the specified chat.
   """
   @spec append(String.t(), String.t(), String.t()) :: :ok
--- test/sketchy_chat/chat_test.exs
+++ test/sketchy_chat/chat_test.exs
@@ -10,22 +10,36 @@ defmodule SketchyChat.ChatTest do
   alias SketchyChat.Chat
 
   setup do
-    # Start the Chat server with a test name
-    {:ok, _pid} = Chat.Server.start_link("test_chat")
-    :ok
+    # Start a test chat
+    {:ok, chat_name} = Chat.new("test_chat")
+    %{chat_name: chat_name}
   end
 
-  test "appending and listing messages" do
-    # Initial state should be empty
-    assert Chat.list("test_chat") == []
+  test "creating a new chat with random name", %{chat_name: _existing_chat} do
+    {:ok, new_chat} = Chat.new()
+    assert is_binary(new_chat)
+    assert String.length(new_chat) == SketchyChat.Utils.ShortCode.length()
+  end
 
-    IO.puts("here 1")
+  test "creating a new chat with specific name", %{chat_name: existing_chat} do
+    # Try to create a chat with a new name
+    {:ok, new_chat} = Chat.new("another_chat")
+    assert new_chat == "another_chat"
+
+    # Try to create a chat with an existing name
+    assert {:error, :already_exists} = Chat.new(existing_chat)
+  end
+
+  test "appending and listing messages", %{chat_name: chat_name} do
+    # Initial state should be empty
+    assert Chat.list(chat_name) == []
+
     # Append a message
-    assert :ok = Chat.append("test_chat", "user1", "Hello!")
-    assert Chat.list("test_chat") == [{"user1", "Hello!"}]
+    assert :ok = Chat.append(chat_name, "user1", "Hello!")
+    assert Chat.list(chat_name) == [{"user1", "Hello!"}]
 
     # Append another message
-    assert :ok = Chat.append("test_chat", "user2", "Hi there!")
-    assert Chat.list("test_chat") == [{"user1", "Hello!"}, {"user2", "Hi there!"}]
+    assert :ok = Chat.append(chat_name, "user2", "Hi there!")
+    assert Chat.list(chat_name) == [{"user1", "Hello!"}, {"user2", "Hi there!"}]
   end
 end
